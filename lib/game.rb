c12c50cc90938dda5125d89c79f8d0a00a40a0a1
Waldo <ben.walsham@meltingwaldo.com>
2011-12-22 14:25:01 +0000

    Assign players to a game with or without a specified team.

--- lib/game.rb
+++ lib/game.rb
@@ -6,17 +6,32 @@ class Game
     @teams = [Team.new, Team.new]
   end
 
-  def join(player, team)
-    team.join(player) if team_valid(team) and !already_joined(team)
+  def join(player, team=nil)
+    team = pick_team(team)
+    team.join(player) unless already_joined(player)
 
     self
   end
-  
+
+  def already_joined(p)
+    @teams.first.already_joined(p) or @teams.last.already_joined(p)
+  end
+
+private
   def team_valid(t)
     @teams.include?(t)
   end
 
-  def already_joined(t)
-    @teams.first.already_joined(t) or @teams.last.already_joined(t)
+  def pick_team(t)
+    team = t
+    unless team_valid(t) and t.players_required
+      if @teams.first.players_required
+        team = @teams.first
+      else @teams.last.players_required
+        team = @teams.last
+      end
+    end
+
+    team
   end
 end
--- spec/game.spec
+++ spec/game.spec
@@ -18,5 +18,24 @@ describe "game" do
 
       t.players.count.should == 1
     end
+
+    it "should assign players with teams" do
+      g = Game.new
+      t1 = g.teams.first
+      g.join(Player.new, t1).join(Player.new, t1)
+      t2 = g.teams.last
+      g.join(Player.new, t2).join(Player.new, t2)
+
+      t1.players.count.should == 2
+      t2.players.count.should == 2
+    end
+
+    it "should assign players without selecting teams" do
+      g = Game.new
+      (1..4).each do |i| g.join(Player.new) end
+
+      g.teams.first.players.count.should == 2
+      g.teams.last.players.count.should == 2
+    end
   end
 end
